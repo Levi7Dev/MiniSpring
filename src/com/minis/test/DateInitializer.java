f00c28aba5c17f47981dd7cfde7071b99620e466
Levi <levitan.dev@gmail.com>
2023-05-03 16:45:48 +0800

    jdbc1

--- src/com/minis/test/DateInitializer.java
+++ src/com/minis/test/DateInitializer.java
@@ -5,6 +5,7 @@ import com.minis.web.WebBindingInitializer;
 import com.minis.web.WebDataBinder;
 
 public class DateInitializer implements WebBindingInitializer{
+    @Override
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(Date.class, new CustomDateEditor(Date.class,"yyyy-MM-dd", false));
     }
--- src/com/minis/test/entity/User.java
+++ src/com/minis/test/entity/User.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 public class User {
     int id = 1;
+    String name = "";
+    Date birthday = new Date();
 
     public int getId() {
         return id;
@@ -13,8 +15,6 @@ public class User {
         this.id = id;
     }
 
-    String name = "";
-
     public String getName() {
         return this.name;
     }
@@ -22,8 +22,6 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-
-    Date birthday = new Date();
 
     public Date getBirthday() {
         return birthday;
